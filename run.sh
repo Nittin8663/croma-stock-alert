9c9d181e38b169da8b799a83899b839f6f80b210
Nittin8663 <90694841+Nittin8663@users.noreply.github.com>
2025-07-03 13:23:45 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 source venv/bin/activate
 while true
 do
-  echo "🔄 Checking stock at $(date)"
-  python3 croma_bot.py
+  echo "🔄 Checking stock at $(date)" | tee -a log.txt
+  python3 croma_bot.py >> log.txt 2>&1
   sleep 60
 done
